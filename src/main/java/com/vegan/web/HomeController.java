fb87df94583f869b4e5b6e07f242a2cef559a035
박우빈 <62981093+yunaluv358@users.noreply.github.com>
2020-06-25 20:43:51 +0900

    first

---
+++ src/main/java/com/vegan/web/HomeController.java
@@ -0,0 +1,23 @@
+package com.vegan.web;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    @GetMapping("/")
+    public String home() {
+        logger.info("홈컨트롤러 진입");
+        return "Home.jsp";
+    }
+}
